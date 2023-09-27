071d87bb60aa7856df388ee28e1b6a43519e4fbe
Inna Antypova <nightallatiel@gmail.com>
2023-09-27 11:49:02 +0300

    add def print_info

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,5 @@
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +7,15 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        request = youtube.channels().list(
+            part="snippet,contentDetails,statistics",
+            id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+        )
+        response = request.execute()
+        print(response)
