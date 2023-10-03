4a3490d6d74df856739ed28366ba3079c036786c
Inna Antypova <nightallatiel@gmail.com>
2023-10-03 17:06:10 +0300

    fix Rinat

--- src/channel.py
+++ src/channel.py
@@ -5,14 +5,12 @@ import json
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YOUTUBE_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
 
-        self.request = Channel.youtube.channels().list(
+        self.request = Channel.get_service().channels().list(
             part="snippet,contentDetails,statistics",
             id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
         ).execute()
@@ -35,7 +33,9 @@ class Channel:
     @classmethod
     def get_service(cls):
         """ Возвращает объект для работы с YouTube API"""
-        return cls.youtube
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
 
     def to_json(self, channel_name: str):
         """ Сохраняет в файл значения атрибутов экземпляра `Channel` """
