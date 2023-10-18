ed8b8a828e9fd5e1be4589afce99e02705b6cc55
Inna Antypova <nightallatiel@gmail.com>
2023-10-18 19:03:21 +0300

    add try\except

--- src/video.py
+++ src/video.py
@@ -1,24 +1,35 @@
-import os
-from googleapiclient.discovery import build
 import json
+from src.MixinAPI import MixinAPI
 
 """ Класс для Video из Youtube"""
 
 
-class Video:
-    def __init__(self, video_id):
+class Video(MixinAPI):
+    def __init__(self, video_id: str):
         """Экземпляр инициализируется id видео. Остальные данные берем из API."""
         self.__video_id = video_id
 
-        self.request = Video.get_service().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.__video_id
-        ).execute()
+        # Получаем объект для работы с API
+        super().get_service()
 
-        self.video_title = self.request['items'][0]['snippet']['title']
-        self.view_count = self.request['items'][0]['statistics']['viewCount']
-        self.like_count = self.request['items'][0]['statistics']['likeCount']
-        self.video_url = f"https://www.youtube.com/watch?v={self.__video_id}"
+        try:
+            # Получаем данные от API по video_id
+            self.request = Video.get_service().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.__video_id
+            ).execute()
+
+            self.title = self.request['items'][0]['snippet']['title']
+            self.view_count = self.request['items'][0]['statistics']['viewCount']
+            self.like_count = self.request['items'][0]['statistics']['likeCount']
+            self.video_url = f"https://www.youtube.com/watch?v={self.__video_id}"
+
+        except IndexError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.video_url = None
+            print("Не верный video_id")
 
     @property
     def video_id(self):
@@ -27,18 +38,11 @@ class Video:
 
     def __str__(self):
         """ Возвращает информацию о названии видео. """
-        return f"{self.video_title}"
+        return f"{self.title}"
 
     def print_info(self):
         """ Выводит информацию о видео"""
         print(json.dumps(self.request, indent=2, ensure_ascii=False))
-
-    @classmethod
-    def get_service(cls):
-        """ Возвращает объект для работы с YouTube API"""
-        api_key: str = os.getenv('YOUTUBE_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        return youtube
 
 
 """ Дочерний класс от класса Video для работы с видео и плейлистом"""
